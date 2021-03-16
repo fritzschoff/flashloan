b08e29b8d3b20924334c8d37a5eeac932b027070
fritzschoff <max.j.f@gmx.de>
2021-03-16 21:12:58 +0100

    update contract

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -2,12 +2,16 @@ pragma solidity ^0.6.0;
 
 import {FlashLoanReceiverBase} from "./aave/FlashLoanReceiverBase.sol";
 import {IERC20} from "./openzeppelin/token/ERC20/IERC20.sol";
-import { ILendingPoolAddressesProvider } from './aave/ILendingPoolAddressesProvider.sol';
+import {
+    ILendingPoolAddressesProvider
+} from "./aave/ILendingPoolAddressesProvider.sol";
 
 // https://docs.aave.com/developers/guides/flash-loans
 contract Flashloan is FlashLoanReceiverBase {
-
-     constructor(ILendingPoolAddressesProvider _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
+    constructor(ILendingPoolAddressesProvider _addressProvider)
+        public
+        FlashLoanReceiverBase(_addressProvider)
+    {}
 
     function executeOperation(
         address[] calldata assets,
@@ -24,36 +28,31 @@ contract Flashloan is FlashLoanReceiverBase {
         return true;
     }
 
-        function startFlashloan() public {
+    function startFlashloan(address[] assets, uint256[] amounts) public {
+        require(
+            assets.length == amounts.length,
+            "Flashloan: Both assets and amounts arrays need to have the same length"
+        );
         address receiverAddress = address(this);
 
-        address[] memory assets = new address[](7);
-        assets[0] = address(0xB597cd8D3217ea6477232F9217fa70837ff667Af); // Kovan AAVE
-        assets[1] = address(0x2d12186Fbb9f9a8C28B3FfdD4c42920f8539D738); // Kovan BAT
-        assets[2] = address(0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD); // Kovan DAI
-        assets[3] = address(0x075A36BA8846C6B6F53644fDd3bf17E5151789DC); // Kovan UNI
-        assets[4] = address(0xb7c325266ec274fEb1354021D27FA3E3379D840d); // Kovan YFI
-        assets[5] = address(0xAD5ce863aE3E4E9394Ab43d4ba0D80f419F61789); // Kovan LINK
-        assets[6] = address(0x7FDb81B0b8a010dd4FFc57C3fecbf145BA8Bd947); // Kovan SNX
+        address[] memory assets = new address[](assets.length);
 
-        uint256[] memory amounts = new uint256[](7);
-        amounts[0] = 1 ether;
-        amounts[1] = 1 ether;
-        amounts[2] = 1 ether;
-        amounts[3] = 1 ether;
-        amounts[4] = 1 ether;
-        amounts[5] = 1 ether;
-        amounts[6] = 1 ether;
+        for (uint8 i = 0; i < assets.length; i++) {
+            assets.push(assets[i]);
+        }
+
+        uint256[] memory amounts = new uint256[](amounts.length);
+
+        for (uint8 i = 0; i < amounts.length; i++) {
+            amounts.push(amounts[i]);
+        }
 
         // 0 = no debt, 1 = stable, 2 = variable
-        uint256[] memory modes = new uint256[](7);
-        modes[0] = 0;
-        modes[1] = 0;
-        modes[2] = 0;
-        modes[3] = 0;
-        modes[4] = 0;
-        modes[5] = 0;
-        modes[6] = 0;
+        uint256[] memory modes = new uint256[](amounts.length);
+
+        for (uint8 i = 0; i < modes.length; i++) {
+            modes.push(0);
+        }
 
         address onBehalfOf = address(msg.sender);
         bytes memory params = "";
